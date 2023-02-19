c5479c893b2c7e227cb354f8becad7f2a3574fbb
Valentina <valya.cherednichenko.03@mail.ru>
2023-02-19 22:41:49 +0300

    uploading imges

--- src/index.js
+++ src/index.js
@@ -5,25 +5,32 @@ import { cwd } from 'node:process';
 import cheerio from 'cheerio';
 import * as utils from './utils.js';
 
+const LoadImages = ($, dirPath) => {
+  Array.from($('img'))
+  .map(link => {
+    const imgSrc = $(link).attr('src');
+    axios.get(imgSrc)
+      .then(imgResponse => {
+        const srcPath = utils.filePaths(dirPath, utils.nameFromLink(imgSrc));
+        fsp.writeFile(srcPath, imgResponse.data, 'utf-8')
+      })
+  });
+}
+
 export default (url, defaultPath = cwd()) => {
   let $;
   return axios.get(url)
     .then(response => {
       const data = response.data;
-      $ = cheerio.load(data);
+      $ = cheerio.load(data); 
       const dirName = utils.buildDirName(url);
       const dirPath = utils.filePaths(defaultPath, dirName)
       fsp.access(dirPath, constants.F_OK)
       .catch(() => {
         fsp.mkdir(dirPath);
       });
-      const imges = Array.from($('img'))
-      .map((el) => {
-        const srcs = $(el).attr('src');
-        const srcValue = utils.filePaths('', utils.nameFromLink(srcs))
-        
-      });
-      
+      LoadImages($, dirPath);
+
       const fileName = utils.buildFileName(url);
       const filePath = utils.filePaths(defaultPath, fileName);
       fsp.writeFile(filePath, data);
@@ -34,6 +41,4 @@ export default (url, defaultPath = cwd()) => {
     .catch(error => {
       throw new Error(error);
     }).finally()
-
-    
 }
