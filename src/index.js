b12349b0ac25995399b9b759ea02f29c5a7dbec7
Valentina <valya.cherednichenko.03@mail.ru>
2023-02-20 02:14:00 +0300

    Download images

--- src/index.js
+++ src/index.js
@@ -6,44 +6,51 @@ import { cwd } from 'node:process';
 import cheerio from 'cheerio';
 import * as utils from './utils.js';
 
-const uploadImages = ($, dirPath) => {
+const images = ($, dirPath) => {
   Array.from($('img'))
-  .map(link => {
-    const imgSrc = $(link).attr('src');
-    axios({
-      method: 'get',
-      url: imgSrc,
-      responseType: 'stream',
-    })
+    .map(link => {
+      const imgSrc = $(link).attr('src');
+      const newSrc = utils.filePaths(dirPath, utils.nameFromLink(imgSrc));
+      axios({
+        method: 'get',
+        url: imgSrc,
+        responseType: 'stream',
+      })
       .then(imgResponse => {
-        const srcPath = utils.filePaths(dirPath, utils.nameFromLink(imgSrc));
-        const filestream = fs.createWriteStream(srcPath);
+        const filestream = fs.createWriteStream(newSrc);
         imgResponse.data.pipe(filestream)
       })
+      $(link).attr("src", newSrc);   
+    });
+  return $.html();
+};
+
+const createDir = (url, defaultPath) => {
+  const dirName = utils.buildDirName(url);
+  const dirPath = utils.filePaths(defaultPath, dirName)
+  fsp.access(dirPath, constants.F_OK)
+  .catch(() => {
+    fsp.mkdir(dirPath);
   });
-}
+  return dirPath
+};
 
 export default (url, defaultPath = cwd()) => {
   return axios.get(url)
     .then(response => {
       const data = response.data;
       const $ = cheerio.load(data); 
-      const dirName = utils.buildDirName(url);
-      const dirPath = utils.filePaths(defaultPath, dirName)
-      fsp.access(dirPath, constants.F_OK)
-      .catch(() => {
-        fsp.mkdir(dirPath);
-      });
-      uploadImages($, dirPath);
+
+      const dirPath = createDir(url, defaultPath)
+      const newHtml = images($, dirPath);
 
       const fileName = utils.buildFileName(url);
       const filePath = utils.filePaths(defaultPath, fileName);
-      fsp.writeFile(filePath, data);
+      fsp.writeFile(filePath, newHtml);
       console.log(filePath); 
       console.log(`\nopen ${filePath}`);
-      return filePath;
     })
     .catch(error => {
       throw new Error(error);
     }).finally()
-}
+};
